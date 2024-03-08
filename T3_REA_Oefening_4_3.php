b035046f6f87c821f3d42500ed5a9191a668d963
CryoCode <owenramaekers2006@gmail.com>
2024-03-08 12:17:26 +0100

    new file:   README.MD 	new file:   T3_REA_Oefening_4_3.php 	new file:   funtions.php 	new file:   styles/style.css

---
+++ T3_REA_Oefening_4_3.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * User: Owen Ramaekers
+ * Date: 7-3-2024
+ * File: T3_REA_Oefening_4_3.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>
+            <?php
+                echo 'Realiseren instructies thema 3 en 4.';
+            ?>
+        </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="styles/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1>
+                <?php
+                    echo 'Instructies';
+                ?>
+            </h1>
+        </header>
+        <?php
+            //Hier gaan we in het volgende hoofdstuk het menu toevoegen.
+        ?>
+        <main id="wrapper">
+            <nav>
+                <h2>Hoofdmenu</h2>
+                <h3>Hoofdstuk 1:</h3>
+                <ul>
+                    <li><a href="/REA_T3T4/Realiseren/Thema3/Hoofdstuk1/Oefening/T3_REA_Oefening1.1.php">Oefening 1.1</a></li>
+                    <li><a href="/REA_T3T4/Realiseren/Thema3/Hoofdstuk1/Oefening/T3_REA_Oefening1.2.php">Oefening 1.2</a></li>
+                </ul>
+            </nav>
+            <section>
+                <h2>
+                    Uitwerking Oefening 4.3
+                </h2>
+                <!-- Plaats je code / uitwerking hieronder -->
+                <p>We zitten nu <?php echo time(); ?> seconden na middernacht 1 januari 1970.</p>
+                
+            </section>
+        </main>
+    </body>
+</html>
+
---
+++ funtions.php
@@ -0,0 +1,8 @@
+/**
+ * User: Owen Ramaekers
+ * Date: 7-3-2024
+ * File: function.php
+ */
+<?php
+
+?>
